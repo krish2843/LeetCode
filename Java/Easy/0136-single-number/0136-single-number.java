f420e4ee334b7a2545f8f0941bb625c8b43caf43
Krishh <krishkumar1147@gmail.com>
2026-09-04 17:20:24 +0530

    Time: 1 ms (99.93%), Space: 47 MB (46%) - LeetHub

--- Java/Easy/0136-single-number/0136-single-number.java
+++ Java/Easy/0136-single-number/0136-single-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int single=0;
-     Arrays.sort(nums);
+   
      for(int i =0;i<nums.length;i++){
         single=single^nums[i];
      }
